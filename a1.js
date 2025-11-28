7ffc7389270075d5c5643a4a94f37ad96d455370
aashikjhajiya-cmd <aashikjhajiya@gmail.com>
2025-11-28 18:54:47 +0530

    Add files via upload

---
+++ a1.js
@@ -0,0 +1,31 @@
+// console.log(2=="2");
+// console.log(typeof(2)=="2");
+// onsole.log(2=="2"); 
+// shift assigment padhna hai
+// let arr = [10, 20, 30, 40, 50];
+// let arr1 = [100, 90, 80, 70, 60]; 
+// const arr2=arr.slice(0,3);
+// console.log(arr2);
+// const arr23=arr.splice(0,3);
+// console.log(arr23);
+// arr1.push(arr)
+// console.log(arr1);
+
+// console.log(arr.concat(arr1));
+
+const r = ramdomnum=Math.random();
+const c=Math.floor(randomnum*100+1);
+let input;
+while(c!=input){
+     input=prompt("enter");
+    
+    
+    if (a>Number(input)) {
+        console.log("enter the lower number");
+    
+    }
+    else if(a>c){
+        console.log("number is higher");
+    }
+    
+}
